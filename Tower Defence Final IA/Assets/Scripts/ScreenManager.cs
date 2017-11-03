eeaea1cd49b21489f4e8736182ab2dd8796f30e5
Sherman Liu <shermanliu03@gmail.com>
2017-11-03 20:30:22 +0800

    Disclaimer

--- Tower Defence Final IA/Assets/Scripts/ScreenManager.cs
+++ Tower Defence Final IA/Assets/Scripts/ScreenManager.cs
@@ -6,7 +6,7 @@ public class ScreenManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Screen.SetResolution (1280, 720, false);
+		Screen.SetResolution (1280, 800, true);
 	
 	}
 	
